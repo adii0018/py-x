4068c44471fbf68e58847e990ecca4ab690ebd88
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-03-03 00:03:09 +0530

    Create constructor_oops.py

---
+++ constructor_oops.py
@@ -0,0 +1,8 @@
+class tube():
+    def __init__(self,brand,price):
+        self.brand=brand
+        self.price=price
+a=tube('Ceat',350)
+b=tube("MRF",797)
+print(a.brand,a.price)
+print(b.brand,b.price)
