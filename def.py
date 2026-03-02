d940dc6374de8d8ccaab9941df4c3cc8ea665348
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-03-02 23:09:18 +0530

    Add cricket_playing_11 class with team print methods

---
+++ def.py
@@ -0,0 +1,9 @@
+class cricket_playing_11:
+    def ind(vk):
+        print("vk,rohit,ms ,hardik, bhumhra, arsh, suriya,aditya ,rahul,khusahal, priti🌿")
+    def pak(ali):
+        print("pK,asskj, akjgks, sghdjksa, dsujsak, hjsabcas, dhjasvc,jkdsac, kjasgcka,🥬")
+    
+a=cricket_playing_11()
+a.ind()
+a.pak()
