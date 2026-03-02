a7f189f73b189b1ed8a86701f6bddab28dec6a9f
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-03-02 23:40:36 +0530

    Implement cricket class with display method
    Added a cricket class to display team names and matches.

--- def.py
+++ def.py
@@ -7,3 +7,16 @@ class cricket_playing_11:
 a=cricket_playing_11()
 a.ind()
 a.pak()
+print("............................................")
+
+class cricket():
+    def __init__(self,name, match):
+        self.name='india'
+        self.match='india  vs pak '
+    def display(self):
+        print(self.name,self.match)
+s1=cricket("18",19)
+s2=cricket("19",871)
+
+s1.display()
+s2.display() 
