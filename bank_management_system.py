219b6bf70f1f84b0c61326998aebfba3a7fe8b47
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-03-04 20:59:27 +0530

    Implement bankaccount class with details display
    Added a bankaccount class with initialization and detail display methods.

--- bank_management_system.py
+++ bank_management_system.py
@@ -1,1 +1,14 @@
+class bankaccount:
+    bank_name="adii bank "
+    def __init__(self,name,acc_no,balance):
+        self.name=name
+        self.__acc_no=acc_no
+        self._balance=balance
+    #show 
+    def show_detail(self):
+       print("Bank name ❄️",bankaccount.bank_name)
+       print("name ❄️",self.name)
+       print("Account NO ❄️",self.__acc_no) 
+       print("Balance ❄️",self._balance)
+       
 
