fd38bf54b582d986b6b66dc81ab9236307b7c5a7
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-03-09 23:38:17 +0530

    Add SavingsAccount class with interest functionality

--- bank_management_system.py
+++ bank_management_system.py
@@ -40,4 +40,59 @@ class Bankaccount:
         return self.__balance
     
 
-        
+#child class uisng inheritance 🌿
+class SavingsAccount(Bankaccount):
+
+ def __init__(self, name, acc_no, balance, interest_rate):
+        super().__init__(name, acc_no, balance)
+        self.interest_rate = interest_rate
+
+def add_interest(self):
+        interest = self.get_balance() * self.interest_rate / 100
+        self.deposit(interest)
+        print("Interest Added:", interest)
+  
+# ---------------- MAIN PROGRAM ----------------🌿
+
+print("🏦 WELCOME TO ASR  BANK")
+
+name = input("Enter Account Holder Name: ")
+acc_no = int(input("Enter Account Number: "))
+balance = float(input("Enter Initial Balance: "))
+account = SavingsAccount(name, acc_no, balance, 5)
+
+while True:
+
+    print("\n------ BANK MENU ------")
+    print("1. Show Account Details")
+    print("2. Deposit Money")
+    print("3. Withdraw Money")
+    print("4. Check Balance")
+    print("5. Add Interest")
+    print("6. Exit")
+
+    choice = int(input("Enter Choice: "))
+
+    if choice == 1:
+        account.show_details()
+
+    elif choice == 2:
+        amount = float(input("Enter Deposit Amount: "))
+        account.deposit(amount)
+
+    elif choice == 3:
+        amount = float(input("Enter Withdraw Amount: "))
+        account.withdraw(amount)
+
+    elif choice == 4:
+        print("Current Balance:", account.get_balance())
+
+    elif choice == 5:
+        account.add_interest()
+
+    elif choice == 6:
+        print("Thank you for using Python Bank")
+        break
+
+    else:
+        print("Invalid Choice")
