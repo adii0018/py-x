6c6ce49cfdfcaed9762b7401076c2eef0cd21f6c
Aditya Singh Rajput <cs23.adityasinghrajput@svceindore.ac.in>
2026-03-11 23:25:00 +0530

    Add cricket player stats and find best player
    Added a tuple for cricket players and their stats, and implemented logic to find the player with the highest runs.

--- tuple.py
+++ tuple.py
@@ -3,3 +3,27 @@ name,age,country=person
 print(name)
 print(age)
 print(country)
+#...................................
+cricket=(
+    ("virat",18,"100"),
+    ("rohit",45,"50"),
+    ("dhoni",7,"60")
+)
+print("cricket list...\n")
+
+runs=0
+player=""
+
+for player in cricket:
+    name,jno,runs=cricket
+    print("player:", name)
+    print("jursy no :",jno)
+    print("Runs", runs)
+    print("--------------")
+    
+    if runs> runs:
+         runs = runs
+         player = name
+
+print("\n⭐ best player:", name)
+print("⭐ normal:", runs)
